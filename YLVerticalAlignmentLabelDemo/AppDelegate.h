684d999aa35803d3f2b6fcbb6d8b39e70b64a3ad
yongliangPeng <1994577317@qq.com>
2016-08-16 16:17:45 +0800

    Initial Commit

---
+++ YLVerticalAlignmentLabelDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLVerticalAlignmentLabelDemo
+//
+//  Created by 彭永亮 on 16/8/16.
+//  Copyright © 2016年 yongliangP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
